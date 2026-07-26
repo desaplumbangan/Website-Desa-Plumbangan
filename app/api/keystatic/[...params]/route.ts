8bf318b5ba605c7c38e976ce54fd5c59687ba2ce
Rafiqi Abyan Rasyadan <rafiqiabyan05@gmail.com>
2026-07-26 22:18:15 +0700

    ubah lagi

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,7 +1,9 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler';
 import config from '../../../../keystatic.config';
 
-export const dynamic = 'force-dynamic'; // <--- TAMBAHKAN LINE INI
+// Baris ini SANGAT KRUSIAL. Tanpa ini, Next.js akan men-cache rute API,
+// sehingga proses penukaran token OAuth akan selalu gagal di Vercel.
+export const dynamic = 'force-dynamic';
 
 export const { POST, GET } = makeRouteHandler({
   config,
