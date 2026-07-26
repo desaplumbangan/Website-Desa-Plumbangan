5102fd4517e31c8dbd4a9ae0f1350d85e62f670b
Rafiqi Abyan Rasyadan <rafiqiabyan05@gmail.com>
2026-07-26 21:47:47 +0700

    tes route ks

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,4 +1,8 @@
-import { makeRouteHandler } from "@keystatic/next/route-handler";
-import config from "../../../../keystatic.config";
+import { makeRouteHandler } from '@keystatic/next/route-handler';
+import config from '../../../../keystatic.config';
 
-export const { POST, GET } = makeRouteHandler({ config });
+export const dynamic = 'force-dynamic'; // <--- TAMBAHKAN LINE INI
+
+export const { POST, GET } = makeRouteHandler({
+  config,
+});
